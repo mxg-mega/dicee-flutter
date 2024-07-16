6018c56cf1877856f08901ad3a3b6361ff817fc5
mxg-mega <68956501+mxg-mega@users.noreply.github.com>
2024-07-16 09:39:07 +0100

    Dice app update

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,9 +16,47 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void rollDice(){
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      children: [
+        Expanded(
+          child: OutlinedButton(
+            onPressed: () {
+              rollDice();
+            },
+            child: Image.asset('image/dice$leftDiceNumber.png'),
+          ),
+        ),
+        Expanded(
+          child: OutlinedButton(
+            onPressed: () {
+              rollDice();
+            },
+            child: Image.asset('image/dice$rightDiceNumber.png'),
+          ),
+        ),
+      ],
+    );
   }
 }
+
+
